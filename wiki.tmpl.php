2721c42a56c8b63d8f9a74e09830d1627efcda9f
lelcp <letcp@protonmail.com>
2017-12-29 14:30:40 +0100

    Updating wiki search page to use secure connection

--- wiki.tmpl.php
+++ wiki.tmpl.php
@@ -1,4 +1,4 @@
-<form id="wiki" method="get" lang="en" action="http://en.opensuse.org/Special:Search?" class="input-group input-group-lg" >
+<form id="wiki" method="get" lang="en" action="https://en.opensuse.org/Special:Search?" class="input-group input-group-lg" >
     <input id="searchbox" type="text" name="search" value="" class="form-control mr-2" lang="en" placeholder="Search openSUSE wiki"/>
     <button class="btn btn-primary mr-2" type="submit">
 	<span class="typcn typcn-lg typcn-zoom-outline"></span>
