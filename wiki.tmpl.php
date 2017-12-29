dc9e27d14ad92124a222641c756fe7a61eb72692
lelcp <letcp@protonmail.com>
2017-12-29 14:28:03 +0100

    Simplifying wiki search page with better functionality

--- wiki.tmpl.php
+++ wiki.tmpl.php
@@ -1,24 +1,5 @@
-<form id="wiki" method="get" lang="en" action="http://en.opensuse.org/index.php?" class="input-group input-group-lg" >
+<form id="wiki" method="get" lang="en" action="http://en.opensuse.org/Special:Search?" class="input-group input-group-lg" >
     <input id="searchbox" type="text" name="search" value="" class="form-control mr-2" lang="en" placeholder="Search openSUSE wiki"/>
-    <input type="hidden" name="fulltext" value="Search" />
-    <input type="hidden" name="title" value="Special%3ASearch" />
-    <input type="hidden" name="ns0" value="1" />
-    <input type="hidden" name="ns2" value="1" />
-    <input type="hidden" name="ns4" value="1" />
-    <input type="hidden" name="ns6" value="1" />
-    <input type="hidden" name="ns100" value="1" />
-    <input type="hidden" name="ns101" value="1" />
-    <input type="hidden" name="ns102" value="1" />
-    <input type="hidden" name="ns103" value="1" />
-    <input type="hidden" name="ns104" value="1" />
-    <input type="hidden" name="ns105" value="1" />
-    <input type="hidden" name="ns106" value="1" />
-    <input type="hidden" name="ns107" value="1" />
-    <input type="hidden" name="ns108" value="1" />
-    <input type="hidden" name="ns122" value="1" />
-    <input type="hidden" name="ns124" value="1" />
-    <input type="hidden" name="ns126" value="1" />
-    <input type="hidden" name="advanced" value="1" />
     <button class="btn btn-primary mr-2" type="submit">
 	<span class="typcn typcn-lg typcn-zoom-outline"></span>
 	<span lang="en" class="hidden-xs-down">Search</span>
