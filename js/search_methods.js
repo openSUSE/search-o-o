76d481426786dc6caf705143521e30aa6e862381
lelcp <letcp@protonmail.com>
2017-12-29 03:03:52 +0100

    Close #1 fixed search bar

--- js/search_methods.js
+++ js/search_methods.js
@@ -1,10 +1,14 @@
-/* Modified version of https://stackoverflow.com/a/10957943*/
+/* Modified version of https://stackoverflow.com/a/10957943 */
+
+/* changes Search Engine */
 
 function changeAction(val){
-  if ($('#searchForm').length > 0) {
-    document.getElementById('searchForm').setAttribute('action', val);
-  }
+	if ($('#searchForm').length > 0) {
+		document.getElementById('searchForm').setAttribute('action', val);
+	}
 }
+
+/* changes Theme */
 
 function changeTheme(val){
 	if (val=="dark") {
@@ -23,71 +27,80 @@ function changeTheme(val){
 
 var saveclass = null;
 
+/* saves search engine in cookie */
+
 function saveSearch(cookieValue)
 {
-    var sel = document.getElementById('sel');
-    saveclass = saveclass ? saveclass : document.body.className;
-    document.body.className = saveclass + ' ' + sel.value;
-    changeAction(cookieValue)
-    setCookie('search', cookieValue, 365);
+		var sel = document.getElementById('sel');
+		saveclass = saveclass ? saveclass : document.body.className;
+		document.body.className = saveclass + ' ' + sel.value;
+		changeAction(cookieValue)
+		setCookie('search', cookieValue, 365);
 }
+
+/* saves theme in cookie */
 
 function saveTheme(cookieValue)
 {
-    var theme = document.getElementById('theme');
-    saveclass = saveclass ? saveclass : document.body.className;
-    document.body.className = saveclass + ' ' + sel.value;
-    changeTheme(cookieValue)
-    setCookie('theme', cookieValue, 365);
+		var theme = document.getElementById('theme');
+		saveclass = saveclass ? saveclass : document.body.className;
+		document.body.className = saveclass + ' ' + sel.value;
+		changeTheme(cookieValue)
+		setCookie('theme', cookieValue, 365);
 }
+
+/* general function for cookie setting */
 
 function setCookie(cookieName, cookieValue, nDays) {
-    var today = new Date();
-    var expire = new Date();
+		var today = new Date();
+		var expire = new Date();
 
-    if (nDays==null || nDays==0)
-        nDays=1;
+		if (nDays==null || nDays==0)
+				nDays=1;
 
-    expire.setTime(today.getTime() + 3600000*24*nDays);
-    document.cookie = cookieName+"="+escape(cookieValue) + ";expires="+expire.toGMTString();
+		expire.setTime(today.getTime() + 3600000*24*nDays);
+		document.cookie = cookieName+"="+escape(cookieValue) + ";expires="+expire.toGMTString();
 }
+
+/* general function for reading cookie */
 
 function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0)  {
-       changeAction(decodeURIComponent(c.substring(nameEQ.length, c.length)));
-       return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-  }
-  return null;
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0)	{
+			 changeAction(decodeURIComponent(c.substring(nameEQ.length, c.length)));
+			 return decodeURIComponent(c.substring(nameEQ.length, c.length));
+		}
+	}
+	return null;
 }
 
+/* function for checking changes in settings on website as they happen */
 
 document.addEventListener('DOMContentLoaded', function() {
-  var theme = document.getElementById('theme');
-  var selectedTheme = readCookie('theme');
-  if ($('#searchForm').length > 0) {
-	  var sel = document.getElementById('sel');
-	  var selectedSearch = readCookie('search');
-	  if (selectedSearch) {
-	    sel.value = selectedSearch;
-	    changeAction(selectedSearch);
-	  }
-  }
-  if (selectedTheme) {
-    theme.value = selectedTheme;
-    changeTheme(selectedTheme);
-  }
+	var theme = document.getElementById('theme');
+	var selectedTheme = readCookie('theme');
+	if ($('#searchForm').length > 0) {
+		var sel = document.getElementById('sel');
+		var selectedSearch = readCookie('search');
+		if (selectedSearch) {
+			sel.value = selectedSearch;
+			changeAction(selectedSearch);
+		}
+	}
+	if (selectedTheme) {
+		theme.value = selectedTheme;
+		changeTheme(selectedTheme);
+	}
 });
 
-//*****************init i18n
+/* i18n initialization */
 
 var lang = new Lang('en');
-//languages setup - please list here all new language packs
+/* list of possible languages, list new languages here */
 window.lang.dynamic('ar', 'js/langpack/ar.json');
 window.lang.dynamic('ast', 'js/langpack/ast.json');
 window.lang.dynamic('bg', 'js/langpack/bg.json');
@@ -117,56 +130,64 @@ window.lang.dynamic('uk', 'js/langpack/uk.json');
 window.lang.dynamic('zh_CN', 'js/langpack/zh_CN.json');
 window.lang.dynamic('zh_TW', 'js/langpack/zh_TW.json');
 
-//change language on click
+/* change language on selection */
+
 $(document).on("change", "lang-select", function() {
-  var languageSelected = $(this).data('value');
-  var languageString = $(this).html();
-  $("body").fadeOut(300, function() {
-    window.lang.change(languageSelected);
-  });
+	var languageSelected = $(this).data('value');
+	var languageString = $(this).html();
+	$("body").fadeOut(300, function() {
+		window.lang.change(languageSelected);
+	});
 
-  return false;
+	return false;
 })
-$(document).ready(function(){
-	var languageCode;
-  var selectedLanguageName;
 
-  if (cookieLanguage === undefined) {
-    try {
-      // try to use navigator.language
-      languageCode = navigator.language.replace("-","_");
-      window.lang.change(languageCode);
-    }
-    catch(err) {
-      // navigator.language is not available
-      if (navigator.language.length > 2) {
-        try {
-          // try with a more general string (for example, if navigator.language is "es-ES" then "es" is tried)
-          languageCode = navigator.language.substring(0,2);
-          window.lang.change(languageCode);
-        }
-        catch(err) {
-          languageCode = "en";
-        }
-      }
-      else {
-        languageCode = "en";
-      }
-    }
-  }
-  else {
-    languageCode = cookieLanguage;
-  }
-  document.getElementById('lang-select').value = languageCode;
+/* apply settings on page load */
+
+$(document).ready(function(){
+	$("input:text:visible:first").focus();
+	var languageCode;
+	var selectedLanguageName;
+
+	if (cookieLanguage === undefined) {
+		try {
+			// try to use navigator.language
+			languageCode = navigator.language.replace("-","_");
+			window.lang.change(languageCode);
+		}
+		catch(err) {
+			// navigator.language is not available
+			if (navigator.language.length > 2) {
+				try {
+					// try with a more general string (for example, if navigator.language is "es-ES" then "es" is tried)
+					languageCode = navigator.language.substring(0,2);
+					window.lang.change(languageCode);
+				}
+				catch(err) {
+					languageCode = "en";
+				}
+			}
+			else {
+				languageCode = "en";
+			}
+		}
+	}
+	else {
+		languageCode = cookieLanguage;
+	}
+	document.getElementById('lang-select').value = languageCode;
 });
+
+/* function for checking headline on news.opensuse.org */
+
 jQuery(function($) {
 $("#rss-feeds").rss(
-  "http://news.opensuse.org/feed",
-  {
-    limit: 1,
-    ssl: true,
-    layoutTemplate: "<div class=\"alert alert-info container text-center\" role=\"alert\">{entries}</div>",
-    entryTemplate: '<a href="{url}">{title}</a>'
-  },
+	"http://news.opensuse.org/feed",
+	{
+		limit: 1,
+		ssl: true,
+		layoutTemplate: "<div class=\"alert alert-info container text-center\" role=\"alert\">{entries}</div>",
+		entryTemplate: '<a href="{url}">{title}</a>'
+	},
 )
 });
